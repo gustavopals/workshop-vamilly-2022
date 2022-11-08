6f8732c27b6be0bfcfb7a27e9be2b3a1e44d91f0
Gustavo Pals <gustavoandrepals@gmail.com>
2022-11-08 17:06:22 -0300

    feat: Tela de dashboard

--- src/app/pages/home/home/home.component.ts
+++ src/app/pages/home/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PoGaugeRanges } from '@po-ui/ng-components';
 
 @Component({
 	selector: 'app-home',
@@ -19,6 +20,18 @@ export class HomeComponent implements OnInit {
 		email: 'gustavo.pals@vamilly.com.br',
 		phone: '42999365350'
 	};
+
+	salesRanges: Array<PoGaugeRanges> = [
+		{ from: 0, to: 50, label: 'Sales reduction', color: '#c64840' },
+		{ from: 50, to: 75, label: 'Average sales', color: '#ea9b3e' },
+		{ from: 75, to: 100, label: 'Sales soared', color: '#00b28e' }
+	];
+
+	turnoverRanges: Array<PoGaugeRanges> = [
+		{ from: 0, to: 50, label: 'Low rate', color: '#00b28e' },
+		{ from: 50, to: 75, label: 'Average rate', color: '#ea9b3e' },
+		{ from: 75, to: 100, label: 'High rate', color: '#c64840' }
+	  ];
 
 	callContact(phone) {
 		window.open(`tel:${phone}`, '_self');
